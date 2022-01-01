ea36e0bd265a914a6cf28a4bf4cbebea1fb6c16e
tomislavvesic <tomislav.vesic1997@gmail.com>
2022-01-02 00:03:47 +0100

    Add updates to template2

--- FreeCVcreator/src/app/templates/template-two/template-two.component.ts
+++ FreeCVcreator/src/app/templates/template-two/template-two.component.ts
@@ -19,5 +19,10 @@ export class TemplateTwoComponent {
   @Input() profileEditor: string = ''
   @Input() jobs: Array<any> = []
 
+  @Input() tempColor: string = 'black'
+  @Input() tempBackground: string = 'white'
+  @Input() personalColor: string = 'white'
+  @Input() personalBackground: string = '#000044'
+
   constructor() { }
 }
